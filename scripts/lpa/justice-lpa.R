0832020c0dfceadeb0f379c847a49a0ffa37fa3a
Kristiina Joon <krikri@staff-net-cx-1129.intern.ethz.ch>
2025-01-08 17:30:47 +0100

    fix lpa bug, add test plot for models 3 to 5

--- scripts/lpa/justice-lpa.R
+++ scripts/lpa/justice-lpa.R
@@ -2,21 +2,22 @@ library(mclust)
 library(tidyLPA)
 # library(nnet)
 library(tidyverse)
-library(dplyr)
+# library(dplyr)
 library(ggplot2)
 library(patchwork)
 library(grid) # needed??
 # library(Matrix)
 
 lpa_data <- read.csv("data/lpa_input.csv")
-set.seed(123)
+set.seed(42)
+m = 10
 
 ################### get LPA models #####################
 
 # run model and store results
 lpa_columns <- lpa_data %>%
-  # filter(country == "CN") %>%
-  select(-id, -country)
+  # filter(country == "US") %>%
+  select(-X, -id, -country)
 lpa_columns <- lpa_columns %>%
   mutate_if(is.factor, as.numeric) %>%
   mutate_if(is.character, as.numeric)
@@ -24,13 +25,14 @@ lpa_columns <- lpa_columns %>%
 # perform lpa
 lpa_results <- estimate_profiles(
   lpa_columns,
-  1:8,
+  1:m,
   # choosing the mclust model EEI for class-invariant parameterization
   variances = "equal",
   covariances = "zero",
   package = "mclust"
 )
 
+#TODO try to work without tidylpa and use purrr to format the results
 # purrr::map_dfr(
 #   lpa_results,
 #   ~ tibble::as_tibble_row(.x$fit),
@@ -42,15 +44,15 @@ lpa_results <- estimate_profiles(
 # summary(lpa_results$model_1_class_1$estimates)
 
 fit_stats <- data.frame(
-  G = 1:8,
-  AIC = sapply(1:8, function(x) lpa_results[[x]]$fit[[4]]),
-  AWE = sapply(1:8, function(x) lpa_results[[x]]$fit[[5]]),
-  BIC = sapply(1:8, function(x) lpa_results[[x]]$fit[[6]]),
-  SABIC = sapply(1:8, function(x) lpa_results[[x]]$fit[[10]]),
-  ICL = sapply(1:8, function(x) lpa_results[[x]]$fit[[11]]),
-  entropy = sapply(1:8, function(x) lpa_results[[x]]$fit[[12]]),
-  prob_min = sapply(1:8, function(x) lpa_results[[x]]$fit[[13]]),
-  prob_max = sapply(1:8, function(x) lpa_results[[x]]$fit[[14]])
+  G = 1:m,
+  AIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[4]]),
+  AWE = sapply(1:m, function(x) lpa_results[[x]]$fit[[5]]),
+  BIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[6]]),
+  SABIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[10]]),
+  ICL = sapply(1:m, function(x) lpa_results[[x]]$fit[[11]]),
+  entropy = sapply(1:m, function(x) lpa_results[[x]]$fit[[12]]),
+  prob_min = sapply(1:m, function(x) lpa_results[[x]]$fit[[13]]),
+  prob_max = sapply(1:m, function(x) lpa_results[[x]]$fit[[14]])
 )
 
 # Calculate proportions for each G
@@ -81,7 +83,7 @@ fit_stats_long <- pivot_longer(fit_stats_plot,
 
 # create elbow plot of fit statistics
 plot <- ggplot(fit_stats_long, aes(x = G, y = Value, color = Statistic)) +
-  geom_line(size = 1) +
+  geom_line(linewidth = 1) +
   geom_point(size = 3) +
   labs(
     title = "Elbow Plot of Fit Statistics for LPA",
@@ -98,10 +100,27 @@ ggsave("output/fit-stats-lpa.png", plot = plot, width = 8, height = 6, dpi = 300
 ###################### select model ###########################
 
 class_assignments_3 <- lpa_results[[3]]$dff$Class
-class_assignments_6 <- lpa_results[[6]]$dff$Class
+class_assignments_5 <- lpa_results[[5]]$dff$Class
 
 lpa_data <- lpa_data %>%
   mutate(justice_class_3 = class_assignments_3) %>%
-  mutate(justice_class_6 = class_assignments_6)
+  mutate(justice_class_5 = class_assignments_5)
 
-write.csv(lpa_data, "output/lpa_output.csv", row.names = TRUE)
+write.csv(lpa_data, "data/lpa_output.csv", row.names = TRUE)
+
+
+################### check models 3 to 5 ######################
+
+lpa_tidy <- lpa_columns %>%
+  single_imputation() %>%    # handle missing data if necessary
+  estimate_profiles(n_profiles = 3:5,
+                    package = "mclust")
+
+# summarize the tidyLPA model
+lpa_tidy
+
+# visualize the profiles from tidyLPA
+plot_profiles(lpa_tidy)
+
+counts <- table(lpa_data$justice_class)
+counts
