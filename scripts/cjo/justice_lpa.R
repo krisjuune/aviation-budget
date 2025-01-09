74e00f5661f4127102f25931ce587958bde31cce
Kristiina Joon <krikri@staff-net-cx-1129.intern.ethz.ch>
2025-01-09 11:44:27 +0100

    add function for running lpa

--- scripts/cjo/justice_lpa.R
+++ scripts/cjo/justice_lpa.R
@@ -8,31 +8,116 @@ library(patchwork)
 library(grid) # needed??
 # library(Matrix)
 
-lpa_data <- read.csv("data/lpa_input.csv")
-set.seed(42)
+lpa_data <- read.csv("data/lpa_input.csv") %>%
+  select(-X)
+set.seed(43)
 
 # set nr of models to test, 1:m models will be checked
-m <- 10
+m <- 8
 
 ################### get LPA models #####################
 
-# run model and store results
+# choose columns for the lpa
 lpa_columns <- lpa_data %>%
-  # filter(country == "US") %>%
-  select(-X, -id, -country)
-lpa_columns <- lpa_columns %>%
-  mutate_if(is.factor, as.numeric) %>%
-  mutate_if(is.character, as.numeric)
+  select(-id, -country)
+
+# also test with CH data only
+lpa_data_ch <- lpa_data %>%
+  filter(country == "CH")
+
+lpa_columns_ch <- lpa_data_ch %>%
+  select(-id, -country)
+
+perform_lpa_analysis <- function(lpa_columns, m, output_dir = "output", country = "") {
+  # check if data is numeric as it should
+  lpa_columns <- lpa_columns %>%
+    mutate_if(is.factor, as.numeric) %>%
+    mutate_if(is.character, as.numeric)
+  
+  # adjust filenames based on country
+  csv_filename <- if (country != "") paste0("lpa_fit_stats_", country, ".csv") else "lpa_fit_stats.csv"
+  png_filename <- if (country != "") paste0("lpa_fit_stats_elbow_", country, ".png") else "lpa_fit_stats_elbow.png"
+
+  # perform LPA using mclust
+  lpa_results <- estimate_profiles(
+    lpa_columns,
+    1:m,
+    variances = "equal",
+    covariances = "zero",
+    package = "mclust"
+  )
+
+  # extract fit statistics
+  fit_stats <- data.frame(
+    G = 1:m,
+    AIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[4]]),
+    AWE = sapply(1:m, function(x) lpa_results[[x]]$fit[[5]]),
+    BIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[6]]),
+    SABIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[10]]),
+    ICL = sapply(1:m, function(x) lpa_results[[x]]$fit[[11]]),
+    entropy = sapply(1:m, function(x) lpa_results[[x]]$fit[[12]]),
+    prob_min = sapply(1:m, function(x) lpa_results[[x]]$fit[[13]]),
+    prob_max = sapply(1:m, function(x) lpa_results[[x]]$fit[[14]])
+  )
+
+  # calculate proportion of smallest class
+  min_proportions <- numeric(length(lpa_results))
+  for (i in seq_along(lpa_results)) {
+    class_assignments <- lpa_results[[i]]$dff$Class
+    class_proportions <- table(class_assignments) / length(class_assignments) * 100
+    min_proportions[i] <- round(min(class_proportions), 1)
+  }
+  fit_stats$min_proportion <- min_proportions
+
+  # write fit statistics to CSV
+  write.csv(fit_stats, file.path(output_dir, csv_filename), row.names = TRUE)
+
+  # prepare data for plotting
+  fit_stats_plot <- fit_stats %>%
+    select(G, AIC, AWE, BIC, SABIC, ICL) %>%
+    mutate(ICL = ICL * -1)
+
+  fit_stats_long <- pivot_longer(fit_stats_plot,
+                                 cols = -G,
+                                 names_to = "Statistic",
+                                 values_to = "Value")
+
+  # generate elbow plot with fit stats
+  plot <- ggplot(fit_stats_long, aes(x = G, y = Value, color = Statistic)) +
+    geom_line(linewidth = 1) +
+    geom_point(size = 3) +
+    labs(
+      title = "Elbow Plot of Fit Statistics for LPA",
+      x = "Number of Profiles (G)",
+      y = "Fit Statistics"
+    ) +
+    theme_minimal() +
+    theme(text = element_text(size = 12))
+
+  # save the plot
+  ggsave(file.path(output_dir, png_filename),
+         plot = plot,
+         width = 8,
+         height = 6,
+         dpi = 300,
+         bg = 'white')
+
+  return(lpa_results)
+}
+
+# running the function takes a couple of mins
+lpa_results <- perform_lpa_analysis(lpa_columns, m)
+lpa_results_ch <- perform_lpa_analysis(lpa_columns_ch, m, country = "ch")
 
 # perform lpa
-lpa_results <- estimate_profiles(
-  lpa_columns,
-  1:m,
-  # choosing the mclust model EEI for class-invariant parameterization
-  variances = "equal",
-  covariances = "zero",
-  package = "mclust"
-)
+# lpa_results <- estimate_profiles(
+#   lpa_columns,
+#   1:m,
+#   # choosing the mclust model EEI for class-invariant parameterization
+#   variances = "equal",
+#   covariances = "zero",
+#   package = "mclust"
+# )
 
 #TODO try to work without tidylpa and use purrr to format the results
 # purrr::map_dfr(
@@ -45,59 +130,17 @@ lpa_results <- estimate_profiles(
 
 # summary(lpa_results$model_1_class_1$estimates)
 
-fit_stats <- data.frame(
-  G = 1:m,
-  AIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[4]]),
-  AWE = sapply(1:m, function(x) lpa_results[[x]]$fit[[5]]),
-  BIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[6]]),
-  SABIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[10]]),
-  ICL = sapply(1:m, function(x) lpa_results[[x]]$fit[[11]]),
-  entropy = sapply(1:m, function(x) lpa_results[[x]]$fit[[12]]),
-  prob_min = sapply(1:m, function(x) lpa_results[[x]]$fit[[13]]),
-  prob_max = sapply(1:m, function(x) lpa_results[[x]]$fit[[14]])
-)
-
-# Calculate proportions for each G
-min_proportions <- numeric(length(lpa_results))
-
-# Get smallest proportions for each model (each G)
-for (i in seq_along(lpa_results)) {
-  class_assignments <- lpa_results[[i]]$dff$Class
-  class_proportions <- table(class_assignments) / length(class_assignments) * 100
-  min_proportions[i] <- round(min(class_proportions), 1)
-}
-
-fit_stats$min_proportion <- min_proportions
-
-# write fit stats to file
-write.csv(fit_stats, "output/lpa_fit_stats.csv", row.names = TRUE)
-
-# reshape the data for plotting
-fit_stats_plot <- fit_stats %>%
-  select(G, AIC, AWE, BIC, SABIC, ICL) %>%
-  mutate(ICL = ICL * -1)
-
-fit_stats_long <- pivot_longer(fit_stats_plot,
-  cols = -G,
-  names_to = "Statistic",
-  values_to = "Value"
-)
-
-# create elbow plot of fit statistics
-plot <- ggplot(fit_stats_long, aes(x = G, y = Value, color = Statistic)) +
-  geom_line(linewidth = 1) +
-  geom_point(size = 3) +
-  labs(
-    title = "Elbow Plot of Fit Statistics for LPA",
-    x = "Number of Profiles (G)",
-    y = "Fit Statistics"
-  ) +
-  theme_minimal() +
-  theme(text = element_text(size = 12))
-
-# Save the plot to a PNG file
-ggsave("output/fit-stats-lpa.png", plot = plot, width = 8, height = 6, dpi = 300)
-
+# fit_stats <- data.frame(
+#   G = 1:m,
+#   AIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[4]]),
+#   AWE = sapply(1:m, function(x) lpa_results[[x]]$fit[[5]]),
+#   BIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[6]]),
+#   SABIC = sapply(1:m, function(x) lpa_results[[x]]$fit[[10]]),
+#   ICL = sapply(1:m, function(x) lpa_results[[x]]$fit[[11]]),
+#   entropy = sapply(1:m, function(x) lpa_results[[x]]$fit[[12]]),
+#   prob_min = sapply(1:m, function(x) lpa_results[[x]]$fit[[13]]),
+#   prob_max = sapply(1:m, function(x) lpa_results[[x]]$fit[[14]])
+# )
 
 ###################### select model ###########################
 
