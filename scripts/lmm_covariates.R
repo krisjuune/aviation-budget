1ed2da9e6860588581281973fe53cb9457e71d8a
krisjuune <kristiina.joon@gmail.com>
2025-05-16 18:09:47 +0200

    added subgroup analysis per income and flying behavious

--- scripts/lmm_covariates.R
+++ scripts/lmm_covariates.R
@@ -16,7 +16,7 @@ source(here("scripts", "lmm_simple_models.R"))
 data_controls <- read_csv(here("data", "wtc_wtp_controls_tidy.csv")) |>
   filter(!is.na(wtc))
 
-################## other variables ####################
+# adding additional covariates
 
 model_wtc <- lmer(
   wtc ~ treatment * red_amt +
@@ -93,5 +93,132 @@ ggsave(
 ggsave(
   plot = plot_wtp_covariates,
   here("output", "plot_wtp_covariates.png"),
+  height = 6, width = 10
+)
+
+# subgroup analysis
+
+data_controls <- data_controls |>
+  mutate(
+    income_group = case_when(
+      income_decile %in% 1:3 ~ "low",
+      income_decile %in% 4:7 ~ "mid",
+      income_decile %in% 8:10 ~ "high",
+      TRUE ~ NA_character_ 
+    ),
+    income_group = factor(income_group, levels = c("low", "mid", "high"))
+  )
+
+model_wtc <- lmer(
+  wtc ~ treatment * income_group +
+    (1 | country),
+  data = data_controls
+)
+
+model_wtp <- lmer(
+  wtp ~ treatment * income_group +
+    (1 | country),
+  data = data_controls
+)
+
+emm_wtc_income <- emmeans(model_wtc, ~ income_group * treatment)
+emm_wtp_income <- emmeans(model_wtp, ~ income_group * treatment)
+
+model_wtc <- lmer(
+  wtc ~ treatment * flying_recent +
+    (1 | country),
+  data = data_controls
+)
+
+model_wtp <- lmer(
+  wtp ~ treatment * flying_recent +
+    (1 | country),
+  data = data_controls
+)
+
+emm_wtc_flyer <- emmeans(model_wtc, ~ flying_recent * treatment)
+emm_wtp_flyer <- emmeans(model_wtp, ~ flying_recent * treatment)
+
+plot_emmeans_subgroup <- function(
+  emm,
+  by,
+  legend_title = "Income group",
+  main_text_size = 14,
+  alpha = 1
+) {
+  data <- as.data.frame(emm)
+  by_sym <- ensym(by)
+
+  ymin_col <- if ("asymp.LCL" %in% names(data)) "asymp.LCL" else "lower.CL"
+  ymax_col <- if ("asymp.UCL" %in% names(data)) "asymp.UCL" else "upper.CL"
+
+  ggplot(data, aes(
+    x = treatment,
+    y = emmean,
+    color = !!by_sym,
+    group = !!by_sym
+  )) +
+    geom_point(position = position_dodge(0.3), size = 3, alpha = alpha) +
+    geom_errorbar(
+      aes(
+        ymin = .data[[ymin_col]],
+        ymax = .data[[ymax_col]]
+      ),
+      width = 0.2,
+      position = position_dodge(0.3),
+      alpha = alpha
+    ) +
+    labs(
+      y = NULL,
+      x = "Treatment",
+      color = legend_title
+    ) +
+    scale_color_viridis_d(option = "plasma", end = .8) +
+    geom_hline(
+      yintercept = 2.5,
+      linetype = 2,
+      colour = "gray40",
+      linewidth = .3
+    ) +
+    theme_classic() +
+    theme(text = element_text(size = main_text_size))
+}
+
+plot_wtc_income <- plot_emmeans_subgroup(emm_wtc_income, by = income_group) +
+  labs(title = "A. Willingness to reduce flying")
+plot_wtp_income <- plot_emmeans_subgroup(emm_wtp_income, by = income_group) +
+  labs(title = "B. Willingness to pay for SAFs")
+
+plot_income_subgroups <- (plot_wtc_income / plot_wtp_income) +
+  plot_layout(guides = "collect", axis_titles = "collect")
+
+plot_wtc_flyer <- plot_emmeans_subgroup(
+  emm_wtc_flyer,
+  by = flying_recent,
+  legend_title = "Flown in the past year"
+) +
+  labs(title = "A. Willingness to reduce flying")
+
+plot_wtp_flyer <- plot_emmeans_subgroup(
+  emm_wtp_flyer,
+  by = flying_recent,
+  legend_title = "Flown in the past year"
+) +
+  labs(title = "B. Willingness to pay for SAFs")
+
+plot_flyer_subgroups <- (plot_wtc_flyer / plot_wtp_flyer) +
+  plot_layout(guides = "collect", axis_titles = "collect")
+
+#TODO add also three-level category for flying, no flights plannes, some, many
+
+ggsave(
+  plot = plot_income_subgroups,
+  here("output", "plot_income_subgroups.png"),
+  height = 6, width = 10
+)
+
+ggsave(
+  plot = plot_flyer_subgroups,
+  here("output", "plot_flyer_subgroups.png"),
   height = 6, width = 10
 )
